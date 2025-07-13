4e461896147b3fe8ab160423eec5b1e315890068
IlhamTessa13 <ilhamts139@gmail.com>
2025-07-13 23:42:32 +0700

    feat: update cors configuration

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [ 
 'paths' => ['api/*'], 
 'allowed_methods' => ['*'], 
-'allowed_origins' => ['http://localhost:5173', 'https://frontend-one-omega-34.vercel.app'], 
+'allowed_origins' => ['http://localhost:5173', 'https://frontend-8uxr.vercel.app'], 
 'allowed_origins_patterns' => [], 
 'allowed_headers' => ['*'],
 'exposed_headers' => [], 
