5e02220d9745ac9f49afd00af5b803e7696c83c7
Rifa Fairuz <222313348@stis.ac.id>
2025-07-13 20:43:35 +0700

    chore: update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Panggil seeder yang ingin Anda jalankan di sini
-        $this->call([
-            DummyUserSeeder::class,
-            // Anda bisa tambahkan seeder lain di sini nanti
-            // ProductSeeder::class,
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
